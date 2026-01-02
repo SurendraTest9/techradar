642c67eb8e10c8e1aea2a35dced2bc8d9718608a
SurendraTest9 <surendra.csh@gmail.com>
2026-01-02 19:53:45 +0530

    Update theme-context.js

--- src/components/theme-context.js
+++ src/components/theme-context.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 //import { scaleOrdinal } from "d3-scale";
 //export { scaleOrdinal } from "d3-scale";
-import * as d3Scales from 'd3-scale-chromatic'
+//import * as d3Scales from 'd3-scale-chromatic'
 
 export const colorScales = [
     {"name": "schemeCategory10"},
