890e9d37f37b758fbe79664b2b8d43ad454ab062
surendra <testlabk8@gmail.com>
2026-01-03 20:04:57 +0530

    radar code

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,9 @@
-// Mock d3-scale before it's required
 jest.mock('d3-scale');
 jest.mock('d3-scale-chromatic');
 
-const d3Scale = require('d3-scale');
-const d3ScaleChromatic = require('d3-scale-chromatic');
-const d3Shim = require('../d3-shim.cjs');
-
-// Inject mocks into the shim
-d3Shim.__setMocks(d3Scale, d3ScaleChromatic);
-
 const React = require('react');
-const { render, screen } = require('@testing-library/react');
+const d3Shim = require('../d3-shim.cjs'); // this will use the mocks
+const { render } = require('@testing-library/react');
 
 const AppModule = require('./App');
 const App = AppModule && AppModule.default ? AppModule.default : AppModule;
