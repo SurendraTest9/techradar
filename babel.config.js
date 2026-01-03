08c4baf1016e7c250df3f9b654ebd875a82fc5ca
surendra <testlabk8@gmail.com>
2026-01-03 11:48:47 +0530

    radar code

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [
-    // transpile to Node current (used when running tests)
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    // if your project uses React (it appears you do), enable this:
-    '@babel/preset-react'
-    // If you use TypeScript, add '@babel/preset-typescript' (and install it)
-  ],
-};
