6a0732599c89f9c5f8b2bafc120e6101cb3c2300
surendra <testlabk8@gmail.com>
2026-01-03 11:57:21 +0530

    radar code

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
   presets: [
+    // transpile to Node current when running tests
     ['@babel/preset-env', { targets: { node: 'current' } }],
+    // if you use JSX
     '@babel/preset-react'
-    // add '@babel/preset-typescript' if you use TypeScript
-  ]
+    // add '@babel/preset-typescript' here if you use TypeScript
+  ],
 };
--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,14 @@
 module.exports = {
+  // Transform JS/TS and .mjs with babel-jest so ESM syntax is handled
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest'
+    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.mjs$': 'babel-jest'
   },
-  // transform d3 ESM packages so Jest won't choke on `export`
+  // By default Jest ignores node_modules. Allow it to transform d3 packages
+  // (adjust the list if you want only specific packages)
   transformIgnorePatterns: [
     '/node_modules/(?!(d3-.*)/)'
   ],
-  testEnvironment: 'jsdom'
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'mjs', 'json', 'node']
 };
